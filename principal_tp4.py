a771abf1a589ede90e276b91756ba88243e05fa2
NicolasCampeau <NICAM8@ulaval.ca>
2022-12-03 16:49:31 -0500

    Emplacement des défi

--- principal_tp4.py
+++ principal_tp4.py
@@ -6,3 +6,9 @@ from interface.fenetre_principale import FenetrePrincipale
 
 fenetre_principale = FenetrePrincipale()
 fenetre_principale.mainloop()
+
+# défi dessiner dé dans canvas_arene ligne 114
+# défi lecture fichier dans fenetre_introduction ligne 206
+# défi tableau des joueurs dans frames_fenetre_principale ligne 102
+# défi temps d'attente dans frames_fenetre_principale ligne 115
+# défi joueur ordinateur dans joueur_ordinateur ligne 11
